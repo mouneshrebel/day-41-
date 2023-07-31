c30812cf54ee8062427fafd420dbd277bdd6ffb4
mounesh <122527299+mouneshrebel@users.noreply.github.com>
2023-07-31 13:53:58 +0530

    Add files via upload

---
+++ B41-Day41/models/mentorSchema.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const mentorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    expertise: {
+        type: String,
+        required: true
+    }
+});
+
+export default mongoose.model('mentor', mentorSchema);
